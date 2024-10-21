43650738ffd7ed62188587931f2f2a55d25eae75
pdtmgrp <pdtmgrp@gmail.com>
2024-10-21 22:52:32 +0530

    Add files via upload

---
+++ Simple2DArray.java
@@ -0,0 +1,15 @@
+public class Simple2DArray {
+    public static void main(String[] args) {
+
+        int[][] numbers = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }
+        };
+
+        System.out.println("2D Array:");
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
