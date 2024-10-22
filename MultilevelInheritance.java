2a46a05ff4847c27804817a7a44763708aa9a180
pdtmgrp <pdtmgrp@gmail.com>
2024-10-22 06:32:40 +0530

    Add files via upload

---
+++ MultilevelInheritance.java
@@ -0,0 +1,28 @@
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Mammal extends Animal {
+    void walk() {
+        System.out.println("Mammal is walking");
+    }
+}
+
+class Dog extends Mammal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class MultilevelInheritance {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        
+        dog.eat();  
+        dog.walk();  
+        dog.bark();
+    }
+}
